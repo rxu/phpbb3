08de9ceab05aa053edb8dfb0a8323cd8b87e8793
rxu <rxu@mail.ru>
2010-07-10 21:25:14 +0800

    [ticket/9721] ACP feature to manage attachments.
    The ACP feature is to manage files attached to posts.
    Abilities to list/download/sort/filter/reassign to new posts/delete files.
    
    PHPBB3-9721

---
+++ phpBB/includes/acp/info/acp_manage_attachments.php
@@ -0,0 +1,39 @@
+<?php
+/** 
+*
+* @package acp
+* @version $Id$
+* @copyright (c) 2005 phpBB Group 
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License 
+*
+*/
+
+/**
+* @package module_install
+*/
+class acp_manage_attachments_info
+{
+	function module()
+	{
+		global $user;
+
+		return array(
+			'filename'	=> 'acp_manage_attachments',
+			'title'		=> 'ACP_ATTACHMENTS',
+			'version'	=> '1.0.0',
+			'modes'		=> array(
+				'attachments'	=> array('title' => 'ACP_MANAGE_ATTACHMENTS', 'auth' => 'acl_a_attach', 'cat' => array('ACP_ATTACHMENTS'))
+			),
+		);
+	}
+
+	function install()
+	{
+	}
+
+	function uninstall()
+	{
+	}
+}
+
+?>
